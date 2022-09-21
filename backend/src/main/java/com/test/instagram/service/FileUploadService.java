86e2a8aabc27741e0109acb3016544e38b4b9323
Seo Heawon <gofltmxj@naver.com>
2022-09-21 16:28:53 +0900

    Delete backend/src directory

--- backend/src/main/java/com/test/instagram/service/FileUploadService.java
+++
@@ -1,9 +0,0 @@
-package com.test.instagram.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileUploadService {
-    public void uploadToLocal(MultipartFile file);
-    public byte[] downloadFromLocal(String pathString);
-    public void deleteAtLocal(String fileName);
-}
